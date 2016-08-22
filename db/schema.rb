09d834a6cd2650396ac5709f7f9251a72cac70c5
Sella Rafaeli <sella.rafaeli@gmail.com>
2016-08-21 22:05:24 -0400

    unique emails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160822014551) do
+ActiveRecord::Schema.define(version: 20160822020405) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160822014551) do
     t.datetime "updated_at",      null: false
     t.string   "profile_pic"
     t.string   "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
