3d481d5f899ad342fd7a15af6682f562db0e5cac
Sella Rafaeli <sella.rafaeli@gmail.com>
2016-08-21 22:01:01 -0400

    progress with users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :users
 
-
   root 'application#hello'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160822004530) do
+ActiveRecord::Schema.define(version: 20160822014551) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "profile_pic"
+    t.string   "password_digest"
   end
 
 end
