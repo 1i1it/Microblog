a55da772476e6147a1b21448937840dd2ba93f00
Lily Mateveyeva <lily.matveyeva@gmail.com>
2016-08-25 17:56:52 -0400

    added home, help, contact, updated application.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   resources :users
   post '/signup',  to: 'users#create'
   root 'application#hello'
+  get    '/help',    to: 'static_pages#help'
+  get    '/about',   to: 'static_pages#about'
+  get    '/contact', to: 'static_pages#contact'
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
+
