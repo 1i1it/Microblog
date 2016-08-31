41d95fc377e6b7d6c3d088677d532b9f9e4549d8
Lily Mateveyeva <lily.matveyeva@gmail.com>
2016-08-31 19:44:15 -0400

    reset password

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'sessions/new'
 
   get 'static_pages/home'
@@ -8,7 +12,7 @@ Rails.application.routes.draw do
   resources :users
   
   post '/signup',  to: 'users#create'
-  root 'users#index'
+  root  'static_pages#home'
   get    '/help',    to: 'static_pages#help'
   get    '/about',   to: 'static_pages#about'
   get    '/contact', to: 'static_pages#contact'
@@ -18,5 +22,6 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
 
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
 
