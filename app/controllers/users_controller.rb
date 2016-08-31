94efa809a6f9ef449c2f545d0e84aad63df4b13f
Sella Rafaeli <sella.rafaeli@gmail.com>
2016-08-31 11:06:54 -0400

    account activation first steps

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,9 +34,12 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         format.html { 
-          log_in @user
-          flash[:success] = "Welcome to the Sample App!"
-          redirect_to @user
+          UserMailer.account_activation(@user).deliver_now
+          flash[:info] = "Please check your email to activate your account."
+          redirect_to root_url
+          # log_in @user
+          # flash[:success] = "Welcome to the Sample App!"
+          # redirect_to @user
         }
         format.json { render :show, status: :created, location: @user }
       else
--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,7 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+
+  resources :account_activations, only: [:edit]
 end
 
